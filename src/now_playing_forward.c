aff04b4f92eccd85f6e531eac1f64fc8101aa2dd
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-04-23 22:42:40 +0200

    Update now_playing_forward.c

--- src/now_playing_forward.c
+++ src/now_playing_forward.c
@@ -1,23 +1,22 @@
+#include <zmk/split/transport/central.h>
+#include <raw_hid/raw_hid.h>
 #include <zmk/event_manager.h>
-#include <raw_hid/events.h>
-#include <zmk/split/slave_request.h>
 
 #if defined(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-ZMK_EVENT_DECLARE(raw_hid_received_event);
-
-static int forward(const zmk_event_t *eh) {
+static int forward_raw_hid(const zmk_event_t *eh) {
     const struct raw_hid_received_event *evt = as_raw_hid_received_event(eh);
-    if (!evt) { return ZMK_EV_EVENT_BUBBLE; }
+    if (!evt) return ZMK_EV_EVENT_BUBBLE;
 
-    /* wrap in our split channel */
-    struct zmk_split_slave_request *req = new_zmk_split_slave_request();
-    req->chan = ZMK_SPLIT_CHANNEL_RAW_HID;
-    memcpy(req->data, evt->data, evt->data_size);
-    req->size = evt->data_size;
-    submit_zmk_split_slave_request(req);
+    struct zmk_split_transport_central_command cmd = {
+        .type = ZMK_SPLIT_TRANSPORT_CENTRAL_CMD_TYPE_USER, // pick an unused command type
+        .data = { .user_data = { /* copy your 32 bytes here */ } }
+    };
+
+    extern struct zmk_split_transport_central *active_transport;
+    active_transport->api->send_command(0, cmd); // 0 for the first peripheral
     return ZMK_EV_EVENT_BUBBLE;
 }
 
-ZMK_LISTENER(raw_hid_forward, forward);
+ZMK_LISTENER(raw_hid_forward, forward_raw_hid);
 ZMK_SUBSCRIPTION(raw_hid_forward, raw_hid_received_event);
-#endif  // CONFIG_ZMK_SPLIT_ROLE_CENTRAL
+#endif
