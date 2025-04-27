0bc004d8b429d8bd791edcdda9f16e4d62dc89b9
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-04-27 12:44:20 +0200

    Update events.h

--- include/raw_hid/events.h
+++ include/raw_hid/events.h
@@ -3,15 +3,13 @@
 #include <zmk/event_manager.h>
 
 struct raw_hid_received_event {
-    uint8_t *data;
-    uint8_t length;
+    uint8_t data[32];   /* ← fixed payload */
+    uint8_t length;     /* always 32 for now */
 };
-
 ZMK_EVENT_DECLARE(raw_hid_received_event);
 
 struct raw_hid_sent_event {
-    uint8_t *data;
+    uint8_t data[32];
     uint8_t length;
 };
-
 ZMK_EVENT_DECLARE(raw_hid_sent_event);
