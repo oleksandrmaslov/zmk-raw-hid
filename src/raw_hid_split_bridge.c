b0add6de08545ca77cd7e1b057f90257fdbe9300
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-07-25 23:22:23 +0200

    Update raw_hid_split_bridge.c

--- src/raw_hid_split_bridge.c
+++ src/raw_hid_split_bridge.c
@@ -13,31 +13,27 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #error "RAW_HID output relay field not configured"
 #endif
 
+#define MAX_PAYLOAD_LEN 32
+
 static int raw_hid_bridge_listener(const zmk_event_t *eh) {
     struct raw_hid_received_event *ev = as_raw_hid_received_event(eh);
     if (!ev) {
         return ZMK_EV_EVENT_BUBBLE;
     }
-
     struct zmk_split_bt_output_relay_event relay = {
         .relay_channel = CONFIG_RAW_HID_SPLIT_CHANNEL,
         .RELAY_SIZE_FIELD = ev->length,
+        .value = 0,
     };
-#ifdef MAX_PAYLOAD_LEN
     if (relay.RELAY_SIZE_FIELD > MAX_PAYLOAD_LEN) {
         relay.RELAY_SIZE_FIELD = MAX_PAYLOAD_LEN;
     }
     memcpy(relay.payload, ev->data, relay.RELAY_SIZE_FIELD);
-#else
-    ARG_UNUSED(relay);
-    ARG_UNUSED(ev);
-#endif
 
     const struct device *dev = device_get_binding(CONFIG_RAW_HID_OUTPUT_RELAY_DEVICE);
     if (dev) {
         zmk_split_bt_invoke_output(dev, relay);
     }
-
     return ZMK_EV_EVENT_BUBBLE;
 }
 
